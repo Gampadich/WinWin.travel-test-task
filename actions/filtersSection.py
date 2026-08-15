1a0bc2bdd5788d04b09f170b362174a273c9f3a5
gampadich <gampadich@gmail.com>
2026-08-15 21:55:34 +0300

    Some corrections

--- actions/filtersSection.py
+++ actions/filtersSection.py
@@ -9,9 +9,13 @@ class FiltersSection:
 
     @allure.step('Clicking see_more_button and pets checkbox button')
     def see_more_and_checkbox_button_click(self):
-        self.see_more_button.wait_for(state="visible", timeout=10000)
-        self.see_more_button.scroll_into_view_if_needed()
-        self.see_more_button.click()
+        self.page.wait_for_timeout(1000)
+        see_more = self.page.locator("button[data-wwt-id='filter__see-more--button']").first
+        if see_more.is_visible():
+            see_more.click(force=True)
+            self.page.wait_for_timeout(500)
+        if hasattr(self, 'pets_checkbox'):
+            self.pets_checkbox.click(force=True)
 
     @allure.step('Checking active checkboxes button')
     def active_checkboxes_button_click(self):
