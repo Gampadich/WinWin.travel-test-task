961c058b4aa9f82f55ea8d261beb4a63766f17e5
Gampadich <Gampadich@users.noreply.github.com>
2026-08-15 10:17:05 +0000

    deploy: 60b284fe981267f8dc6c50d8d797698faa1e11db

--- conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-@pytest.fixture(scope='session')
-def url():
-    return 'https://winwin.travel/'
-
-@pytest.fixture(scope="session")
-def browser_context_args(browser_context_args):
-    return {**browser_context_args, "viewport": {"width": 1920, "height": 1080}}
