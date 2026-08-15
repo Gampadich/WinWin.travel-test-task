39accc74214cccc386a88e6c621f73be52361ea6
gampadich <gampadich@gmail.com>
2026-08-15 09:09:22 +0300

    Complete all tests

--- tests/WinWin_travel_test.py
+++ tests/WinWin_travel_test.py
@@ -3,6 +3,7 @@ from playwright.sync_api import expect
 from actions.adultsSection import AdultsSection
 from actions.mainPage import MainPage
 from actions.filtersSection import FiltersSection
+import re
 
 @allure.feature('Main page testing')
 @allure.story('Max Adults section')
@@ -26,3 +27,17 @@ def test_active_pets_checkboxes(url, page):
     filtersSection.see_more_and_checkbox_button_click()
     activeCheckboxes = filtersSection.active_checkboxes_button_click()
     assert len(activeCheckboxes) == 23, f'Checkboxes not active active: {activeCheckboxes}'
+
+@allure.feature('Main page testing')
+@allure.story('Check page url after filters')
+@allure.severity(allure.severity_level.CRITICAL)
+def test_page_url_after_filters(url, page):
+    mainPage = MainPage(page)
+    page.goto(url)
+    cookie_button = page.get_by_role("button", name="Accept all")
+    if cookie_button.is_visible():
+        cookie_button.click()
+        page.wait_for_timeout(300)
+    mainPage.click_all_filter_buttons()
+    mainPage.click_search_button()
+    page.wait_for_url(re.compile(r"search\.filters%5B0%5D\.optionIDs%5B0%5D=2"), timeout=15000)
